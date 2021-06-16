6e39bf7627a04a278a0ce249cd0e1e4f779a23c3
tayyabhussain2910 <50795716+TayyabHussain03@users.noreply.github.com>
2021-06-16 23:19:51 +0500

    calculator completed

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 function submitData(num) {
     var xyz = document.getElementById('fn')
-    xyz.value += num
+    var lastVal = xyz.value.slice(xyz.value.length - 1)
+    var operators = ['*', '/', '-', '+']
+    if (operators.indexOf(lastVal) !== -1 && operators.indexOf(num) !== -1) {
+        xyz.value = xyz.value.slice(0, - 1) + num
+    }
+    else {
+        xyz.value += num
+    }
 }
 
 function clearResult() {
